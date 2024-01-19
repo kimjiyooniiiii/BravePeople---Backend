cf5431e2a8692f4f1e330492685373e0f85f40ce
taeho <rktlrhrl531@naver.com>
2024-01-19 22:39:57 +0900

    [feat] GET /member/profile 프로필 페이지 반환 개발 완료

--- src/main/java/com/example/brave_people_backend/controller/MemberController.java
+++ src/main/java/com/example/brave_people_backend/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.brave_people_backend.controller;
 import com.example.brave_people_backend.dto.LocationRequestDto;
 import com.example.brave_people_backend.dto.LocationResponseDto;
 import com.example.brave_people_backend.dto.MemberResponseDto;
+import com.example.brave_people_backend.dto.ProfileResponseDto;
 import com.example.brave_people_backend.service.MemberService;
 import com.example.brave_people_backend.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
@@ -27,8 +29,13 @@ public class MemberController {
     }
 
     //위치 변경
-    @PatchMapping("/member/location")
+    @PatchMapping("/location")
     public ResponseEntity<LocationResponseDto> updateLocation(@RequestBody LocationRequestDto locationRequestDto) {
         return ResponseEntity.ok(memberService.updateLocation(locationRequestDto));
+    }
+
+    @GetMapping("/profile")
+    public ProfileResponseDto getProfileInfo(@RequestParam("memberid") Long memberId) {
+        return memberService.getProfileInfo(memberId);
     }
 }
--- src/main/java/com/example/brave_people_backend/dto/ProfileResponseDto.java
+++ src/main/java/com/example/brave_people_backend/dto/ProfileResponseDto.java
@@ -1,9 +1,11 @@
 package com.example.brave_people_backend.dto;
 
+import com.example.brave_people_backend.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 
 @Getter
 @Builder
@@ -16,5 +18,21 @@ public class ProfileResponseDto {
     private String gender;
     private String introduction;
     private double score;
+    private int medalCount;
+    private String profileImage;
+    private Long memberId;
+
+    public static ProfileResponseDto of(Member member) {
+        return ProfileResponseDto.builder()
+                .username(member.getUsername())
+                .nickname(member.getNickname())
+                .gender(member.isGender() ? "여성" : "남성")
+                .introduction(member.getIntroduction())
+                .score(3.5)
+                .medalCount(11)
+                .profileImage("https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg")
+                .memberId(member.getMemberId())
+                .build();
+    }
 
 }
