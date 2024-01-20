8c6a02dbbcae7a2ca972a9553a40991d0b0b84b1
NAYOUNG <yny3533@naver.com>
2024-01-20 20:26:32 +0900

    [refactor] 주석, 위치 변경 및 아이디 찾기 예외 처리 추가

--- src/main/java/com/example/brave_people_backend/service/MemberService.java
+++ src/main/java/com/example/brave_people_backend/service/MemberService.java
@@ -40,8 +40,11 @@ public class MemberService {
     @Transactional
     public LocationResponseDto updateLocation(LocationRequestDto locationRequestDto) {
 
-        Member findMember = memberRepository.findByIdOne(SecurityUtil.getCurrentId());
+        //토큰으로 현재 회원 검색, 없으면 예외처리
+        Member findMember = memberRepository.findById(SecurityUtil.getCurrentId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다."));
 
+        //위도, 경도 변경
         findMember.changeLatAndLng(new BigDecimal(locationRequestDto.getLat()), new BigDecimal(locationRequestDto.getLng()));
 
         return LocationResponseDto.of(findMember);
