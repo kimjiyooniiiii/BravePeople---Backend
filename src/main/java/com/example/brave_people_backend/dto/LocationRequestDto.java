dfc508ffd21f71db1cff7180592ef03b3cbf4d68
NAYOUNG <yny3533@naver.com>
2024-01-23 00:17:06 +0900

    [feat] 위도, 경도 입력 검증 추가

--- src/main/java/com/example/brave_people_backend/dto/LocationRequestDto.java
+++ src/main/java/com/example/brave_people_backend/dto/LocationRequestDto.java
@@ -2,6 +2,8 @@ package com.example.brave_people_backend.dto;
 
 import com.example.brave_people_backend.entity.Member;
 import com.example.brave_people_backend.security.SecurityUtil;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +18,9 @@ import java.math.BigDecimal;
 public class LocationRequestDto {
 
     //request body에서 위도, 경도 정보를 String으로 받음
+    @NotBlank @Pattern(regexp = "(^\\d+$)|(^\\d+\\.\\d{1,15}$)", message = "잘못된 위치 정보")    //정수부 세 자리, 소수점 이하 15자리까지
     private String lat;
+    @NotBlank @Pattern(regexp = "(^\\d+$)|(^\\d+\\.\\d{1,15}$)", message = "잘못된 위치 정보")
     private String lng;
 
 }
