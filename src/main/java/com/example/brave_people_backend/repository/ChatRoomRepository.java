caaaa5c323bcf590427fc34e304335e9ca4fccba
nayoung <yny3533@naver.com>
2024-02-08 16:38:39 +0900

    [feat] 의뢰 생성 기능 개발

--- src/main/java/com/example/brave_people_backend/repository/ChatRoomRepository.java
+++ src/main/java/com/example/brave_people_backend/repository/ChatRoomRepository.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
 
+    //기존에 생성된 채팅방이 있는지 찾음
     @Query("select r.chatRoomId from ChatRoom r where (r.memberA.memberId = :aMemberId and r.memberB.memberId = :bMemberId)" +
             "or(r.memberA.memberId = :bMemberId and r.memberB.memberId = :aMemberId)")
     Long findChatRoom(Long aMemberId, Long bMemberId);
